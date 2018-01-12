1a9eacac05f6832ce5a0abffb144298ccb983ec6
J <flatironschool@2joyce.com>
2018-01-12 23:58:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i < musicians.length; i++0) {
     myArray.push(`${musicians[i]} plays ${instruments[i]}`);
   }
-  
-})
+  return myArray;
+}
